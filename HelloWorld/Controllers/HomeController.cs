c3dc7af1f476d944d7c5bf41638e5ae1761adbad
Cory <sharepoint@live.com>
2021-07-15 19:30:24 -0700

    adding new hello world

---
+++ HelloWorld/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HelloWorld.Models;
+
+namespace HelloWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        private IProductRepository productRepository;
+
+        public HomeController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+
+        [HttpGet]
+        public ActionResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RsvpForm(Models.GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+
+        public ActionResult Product()
+        {
+            var myProduct = productRepository.Products.First();
+            return View(myProduct);
+       
+        }
+
+        public ActionResult Products()
+        {
+            var products = productRepository.Products;
+            return View(products);
+        }
+    }
+}
---
+++ HelloWorld/Models/GuestResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace HelloWorld.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public bool? WillAttend { get; set; }
+
+        public string email { get; set; }
+    }
+}
